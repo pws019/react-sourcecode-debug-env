42379bd994959c0f3243e1578ee73fe6fdda1648
baiyang <xiuer.by@autonavi.com>
2019-08-30 15:05:02 +0800

    配置react调试环境成功

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+console.log('React Version', React.version);
+
 function App() {
   return (
     <div className="App">
